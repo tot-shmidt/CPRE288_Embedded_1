3ef7c11d4756666d9585d020896569df111dcf90
Ziabkin <yarik@iastate.edu>
2026-09-21 21:24:24 -0500

    First commit on part5 branch with some progress

--- lab_4/main.c
+++ lab_4/main.c
@@ -2,24 +2,91 @@
 #include "cyBot_Scan.h"
 #include <stdio.h>
 #include "Timer.h"
+#include "sensor-data.h"
 
+#define NUM_OF_SCANS 91
+
+/**
+ * This represents an object detected in detect_objects();
+ */
+struct tall_object {
+    int start_angle;
+    int end_angle;
+    int radial_width;
+    float linear_width;
+    float distance_to_obj;
+};
+
+/**
+ * This function detects objects, their distances and widths.
+ */
+void detect_objects(float scan_array[]) {
+    float current_distance;
+    float limit_distance = 3.0;
+
+    int i;
+    for (i = 0; i < NUM_OF_SCANS; i++) {
+        current_distance = scan_array[i];
+
+        if (current_distance >= limit_distance) {
+
+        }
+    }
+}
+
+/*
+ * Print the array from sensor_data.h in the required format to a terminal.
+ */
+void display_scan_to_terminal() {
+    int angle_increase = 2;
+
+    printf("%s %s", "Angle(Degrees)", "Distance(m)\n");
+
+    int i, angle;
+    for (i = 0, angle = 0; i < NUM_OF_SCANS; i++, angle += angle_increase) {
+        printf("%-14d %.2f\n", angle, sensor_data_array[i]);
+    }
+}
+
+/**
+ * Clean data which we got from a call to perform_scan()
+ */
+void clean_scanner_data(float scan_array[]) {
+    float delta_limit = 0.15;                           // What delta do we consider as an outlier reading
+
+    float current_distance;                             // Current value from the scan_array
+
+    int i;
+    for (i = 1; i < NUM_OF_SCANS - 1; i++) {            // we process indexes from 1 to 89, as 0 and 90 don't have prev/next readings.
+        current_distance = scan_array[i];
+
+        // 1. Clean 1: Check for outlier points and normalize them by averaging it to their neighbors
+        if (((current_distance - scan_array[i+1] > delta_limit) && (current_distance - scan_array[i-1] > delta_limit)) ||
+            ((scan_array[i+1] - current_distance > delta_limit) && (scan_array[i-1] - current_distance > delta_limit))) {
+
+            current_distance = (scan_array[i-1] + scan_array[i+1]) / 2.0;
+            scan_array[i] = current_distance;
+        }
+    }
+}
+
+
+/*
+ * Perform ultra-sonic scans on 180 degrees range and save distances to scan_array.
+ */
 void perform_scan(float scan_array[], int angle_increment, cyBOT_Scan_t* scanStruct) {
-    int num_of_scans = (180 / angle_increment) + 1;   // We will have 90 scans total.
-
     int i, current_angle;
 
-    cyBOT_scan(0, scanStruct);                    // We do one scan at 0 degrees and discard the reading. This is for the sensor to settle at 0.
+    cyBOT_Scan(0, scanStruct);                      // We do one scan at 0 degrees and discard the reading. This is for the sensor to settle at 0.
 
-    for (i = 0, current_angle = 0; i < num_of_scans; i++, current_angle += angle_increment) {
-       cyBOT_Scan(current_angle, scanStruct);     // Perform scan
+    for (i = 0, current_angle = 0; i < NUM_OF_SCANS; i++, current_angle += angle_increment) {
+       cyBOT_Scan(current_angle, scanStruct);       // Perform scan
 
        *(scan_array + i) = scanStruct->sound_dist;  // Save measured distance to scan_array
     }
 }
 
 void send_scan_to_putty(float scan_array[], int angle_increment) {
-    int num_of_scans = (180 / angle_increment) + 1;
-
     // Print header
     char header[] = "Degrees    Distance(cm)\r\n";
     int i = 0;
@@ -32,7 +99,7 @@ void send_scan_to_putty(float scan_array[], int angle_increment) {
     // Print data
     char destination[50];
 
-    for (i = 0; i < num_of_scans; i++) {
+    for (i = 0; i < NUM_OF_SCANS; i++) {
         sprintf(destination, "%-11d%.1f\r\n", i * angle_increment, *(scan_array + i));
 
         int j = 0;
@@ -55,7 +122,7 @@ int main(void) {
     left_calibration_value = 1230250;           // Servo calibration for 180 degrees
 
     int angle_increment = 2;                    // How often do we scan. Every 2 degrees in this case.
-    float scan_array[91];    // Array where we will store readings from scanStruct.
+    float scan_array[91];                       // Array where we will store readings from scanStruct.
 
     // Debug
     cyBot_sendByte('A');
@@ -64,8 +131,21 @@ int main(void) {
         char input = cyBot_getByte();
 
         if (input == 'm') {
+            // 1. Perform scan and save data to scan_array.
             perform_scan(scan_array, angle_increment, &scanStruct);
+
+            // 2. Clean the data.
+            clean_scanner_data(scan_array);
+
+            // 3. Detect objects from the data and calculate their radial and liner width
+            detect_objects(scan_array);
+
+            // 4. Print whole array and object structs to the terminal/putty, depending on the phase of development
             send_scan_to_putty(scan_array, angle_increment);
+
+            // 5. Point the sensor to the object with the smallest width.
+
+
         }
     }
 
